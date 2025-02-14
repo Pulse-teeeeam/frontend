c460e066b2984a1f842f2dfae50b7d605327e638
What-XD <101012824+What-XD@users.noreply.github.com>
2025-02-14 12:58:45 +0500

    fix

---
+++ src/lib/apollo-client.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ReactNode } from "react";
+
+export default function CustomApolloProvider({ children }: { children: ReactNode }) {
+    const httpLink = createHttpLink({
+      uri: 'http://localhost:8000/graphql/',
+    });
+    
+    const authLink = setContext((_, { headers }) => {
+      const token = localStorage.getItem('token');
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+        }
+      }
+    });
+    
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+    });
+
+    return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
