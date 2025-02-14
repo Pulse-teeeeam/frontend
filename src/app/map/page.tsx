2a4c26ac40e30ca5d298d20b9014399233f7c3c0
What-XD <101012824+What-XD@users.noreply.github.com>
2025-02-14 20:25:18 +0500

    update

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,4 +1,6 @@
 export default function PageMap() {
-    return <></>
+    return <>
+    <iframe src="https://geois2.orb.ru/resource/8898/display/tiny?base=basemap_0&lon=55.5990&lat=52.4813&angle=0&zoom=7&styles=7986%2C7975%2C2092&linkMainMap=true&events=false&panel=none&controls=&panels=" className="w-full h-full" style={{overflow: 'hidden', height: '600px', width: '800px'}}></iframe>
+    </>
 
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,32 @@ import Link from "next/link";
 export default function Home() {
   return <div className="space-y-8">
     <div className="h-[60svh] flex flex-col justify-center">
-      <div className="flex justify-between items-center">
-        <div className="space-y-3">
-          <h1 className="font-bold text-4xl">Книга памяти<br/>Оренбургской области</h1>
+      <div className="flex justify-center md:justify-between items-center">
+        <div className="block md:hidden absolute -z-10">
+          <Image src="/banner.jpg" alt='' className="h-screen object-cover" width={800} height={800}/>
+        </div>
+        <div className="space-y-3 text-center md:text-left bg-white/60 backdrop-blur-2xl p-6 rounded-2xl md:bg-zinc-100">
+          <h1 className="font-bold text-4xl text-black">Книга памяти<br/>Оренбургской области</h1>
           <h3 className="text-xl text-black/80">Никто не забыт, ничто не забыто</h3>
           <button className="bg-indigo-600 text-white px-4 py-2 rounded-md mt-3">Открыть книгу</button>
         </div>
-        <div>
+        <div className="hidden md:block">
           <Image src="/banner.jpg" alt='' className="rounded-2xl" width={600} height={600}/>
         </div>
       </div>
     </div>
     <div>
-      <Link href={'/map'} className="relative group/item">
+      <Link href={'/map'} className="relative group/item text-center">
         <h3 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-black/30 p-3 rounded-xl backdrop-blur-2xl font-bold text-4xl z-30">Перейти на интерактивную карту</h3>
         <img src="https://oren.ru/wp-content/uploads/2017/07/Screenshot_2-2.jpg" alt="" className="w-full object-cover h-[250px] rounded-2xl duration-150 group-hover/item:blur-xs active:scale-90"/>
       </Link>
     </div>
-    <div className="mx-auto max-w-screen-xl bg-white text-black px-8 py-8 rounded-xl grid grid-cols-[35%_65%]" id="form-find">
-      <h3 className="text-4xl font-medium">Найти своего<br/>героя</h3>
+    <div className="mx-auto max-w-screen-xl bg-white text-black px-8 py-8 rounded-xl grid md:grid-cols-[35%_65%]" id="form-find">
+      <h3 className="text-4xl font-medium mb-6 text-center md:text-left md:mb-0">Найти своего<br/>героя</h3>
       <div className="space-y-3">
         <InputUI title="Имя"/>
         <InputUI title="Фамилия"/>
-        <ButtonUI text="Поиск" className="mt-3"/>
+        <ButtonUI text="Поиск" className="mt-3 w-full"/>
       </div>
     </div>
     </div>
