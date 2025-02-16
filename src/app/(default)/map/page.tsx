330155d94cd1e42759c27083b6e04b97ab5e4b0f
What-XD <101012824+What-XD@users.noreply.github.com>
2025-02-16 18:45:53 +0500

    update

--- src/app/(default)/map/page.tsx
+++ src/app/(default)/map/page.tsx
@@ -1,44 +1,36 @@
 'use client'
 import Script from "next/script"
-import geo_borders from './9.json'
+import { useEffect } from "react";
 
-declare var ymaps: any;
+declare var NgwMap: any;
 
 export default function PageMap() {
   const initYmaps = () => {
-    if (typeof ymaps !== "undefined") {
-      var myMap = new ymaps.Map("map", {
-        center: [51.768202, 55.096996],
-        zoom: 10,
-      });
-  
-      console.log(geo_borders);
-      geo_borders.features.forEach((feature) => {
-        const polygon = new ymaps.GeoObject(
-          {
-            geometry: {
-              type: "Polygon",
-              coordinates: feature.geometry.coordinates[0],
-            },
-            properties: {hintContent: feature.properties.name},
-          }
-        );
-        myMap.geoObjects.add(polygon);
-        console.log(polygon)
-      });
-    } else {
-      console.error("Yandex Maps API not loaded");
-    }
+    NgwMap.create({
+      baseUrl: 'https://geois2.orb.ru/resource/8800',
+      target: 'map',
+      center: [51.768205, 55.097000],
+      auth: {
+          login: "hackathon_25",
+          password: "hackathon_25_25"
+      },
+      zoom: 12,
+      adapterOptions: {
+          selectable: !0
+      }
+    });
+
   };
-  
+
 
   return (
     <>
       <Script
-        onLoad={() => ymaps.ready(initYmaps)}
-        src={`https://api-maps.yandex.ru/2.1/?apikey=06839ef3-ad7a-480e-8499-e1818b1c744c&lang=ru_RU`}
+        src={`https://unpkg.com/@nextgis/ngw-maplibre-gl`}
+        onLoad={() => initYmaps()}
+        onError={() => console.error("Script failed to load")}
       />
-      <div id="map" className="w-full h-full"></div>
+      <div id="map" className="w-full h-full" style={{ height: '500px' }}></div>
     </>
   );
 }
